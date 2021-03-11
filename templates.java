6c1cf3f5f3049d3dba3b32f4af35db18db11452b
liukunli <57381324+liukunli@users.noreply.github.com>
2021-03-11 00:01:37 -0500

    update

--- templates.java
+++ templates.java
@@ -1,3 +1,4 @@
+
 DP template: 
 
 1.	High level idea on recursion
@@ -13,15 +14,23 @@ DP complexity:
 Knapsack template, O(n^2) time, O(n) space:
 
 int[] dp = new int[target+1];   // int array or boolean array
-dp[0] =        // initialization
+dp[0] =        // initialization for base caxe
 for(num : nums) {
 	for(; ; ;) {// find the condition that meets the requirement
-			// elements can be repeated infinite times, write for loop from the front to the end
-			// elements can only be use for one time, write for loop from the end to the front
+		      // elements can be repeated infinite times, write for loop from the front to the end
+		      // elements can only be use for one time, write for loop from the end to the front
 	   // dp implementation
 	}
 }
 return dp[target]
+
+Possible variations:
+
+1.	Elements can be repeated or can only be used once for knapsack problem
+2.	For grid problem, there might be variations that there are some obstacles in certain grids
+
+
+
 
 DFS template: 
 
@@ -41,12 +50,15 @@ public void dfs_traverse(TreeNode root) {
 possible variations:
 
 1. order of doing dfs on left, right and on processing root elements
-2. different function on processing root element
+2. different functions on processing root element
+3. do backtracking with DFS to generate paths. Backtrack: choose one option, backtrack_dfs, then undo choosing that option
+
+
 
 Backtrack template:
 
 backtrack() {
-1.	check base case, return
+1.	check base case, return.
 
 for(all possible options) {
 		1.	check if it is a valid option. If not, break;
@@ -59,6 +71,9 @@ for(all possible options) {
 private List<List<Integer>> res = new ArrayList<>();
 private List<Integer> current_list = new ArrayList<>();
 res.add(new ArrayList(current_list));
+
+Notes: in some case other than the base case, you can exit the function directly
+
 
 
 BFS template: 
@@ -74,6 +89,33 @@ BFS template:
 		}
 	}
 }
+
+Possible variations:
+
+1.	Count the number of nodes in each level with a for loop
+2.	Add the left element first into the queue, or the right element first into the queue
+3.	For the first node or the last node in each level, do something special
+4.	For all element on a specific level, do something special. If this operations will require the cooperation from its parents, then just traverse to its parents
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 Binary search template:
@@ -110,6 +152,9 @@ Binary search right bound:
 7.	if(right <0|| nums[right] != target), return -1
 8.	return right
 
+
+
+
 Sliding window minimum template:
 
 left = 0, right;
@@ -124,6 +169,12 @@ for(right = 0; right < len; right++) {
 }
 
 
+The window could be: 
+
+1.	int[26], int [128], int[256]
+2.	HashSet
+3.	HashMap
+
 Sliding window maximum template:
 
 left = 0, right;
@@ -137,6 +188,9 @@ for(right = 0; right < len; r++) {
 	// after the this while loop, the window is valid again. 
 	use Math.max to update max
 }		
+
+
+
 
 Greedy, activity selection template:
 
@@ -154,13 +208,47 @@ for (int i = 1; i < intervals.length; i++) {
 }
 
 
+Possible variations:
+
+1.	Count the maximum number of overlapping activities
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Binary tree template: 
 
 function(root):
 
 1.	check base case. 
-2.  process the root
+2.       	process the root
 3.	recursive calls: function(root.left), function(root.right)
+
+
+LinkedList template: 
+
+function(node) :
+
+1.	check base case.
+2.       	process current node, and possibly process node.next or node.next.next
+3.	recursive calls: function(node.next) or function(node.next.next)
+
+Note for binary tree and LinkedList template: if you need to do operations or modification on a particular node and you need the node before it to do that operation, then just get to the node before it when traversing. The node in tree and LinkedList can only point to the next node. It cannot point to its parents. 
+
+
 
 Next greater number template, using stack:
 
@@ -171,6 +259,82 @@ for(int i = 0; i < len; i++) {
     }
     stack.add(nums[i]);
 }
+
+
+
+
+
+
+Binary tree preorder traversal:
+
+Stack<Node> stack = new Stack<>();
+List<Integer> res = new ArrayList<>();
+
+if(root == null) {
+    return res;
+}
+
+stack.push(root);
+while(!stack.isEmpty()) {
+    Node current = stack.pop();
+    res.add(current.val);
+    if(current.left != null) {
+        stack.push(current.left);
+    }
+    if(current.right != null) {
+        stack.push(current.right);
+    }
+}
+return res;
+
+Note on this template: it is quite similar to BFS template. The difference is that this one is stack. BFS is using queue. 
+
+
+Binary tree postorder traversal:
+
+Stack<Node> stack = new Stack<>();
+List<Integer> res = new ArrayList<>();
+
+if(root == null) {
+    return res;
+}
+
+stack.push(root);
+while(!stack.isEmpty()) {
+    Node current = stack.pop();
+    res.add(current.val);
+    if(current.right != null) {
+        stack.push(current.right);
+    }
+    if(current.left != null) {
+        stack.push(current.left);
+    }
+
+}	
+Collections.reverse(res);
+return res;
+
+Binary tree in-order traversal: 
+
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> ret = new ArrayList<>();
+    if (root == null) return ret;
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode cur = root;
+    while (cur != null || !stack.isEmpty()) {
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        TreeNode node = stack.pop();
+        ret.add(node.val);
+        cur = node.right;
+    }
+    return ret;
+}
+
+
+
 
 Double pointer template:
 
@@ -190,11 +354,30 @@ while(left < right) {
 	}
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Triple pointer template:
 
 	triple pointer: left, current, right
 
-	Segementation: 0, left, current, right, len - 1;
+	Segmentation: 0, left, current, right, len - 1;
 
 	0 to left: small numbers
 	Left to current: middle numbers
@@ -222,13 +405,10 @@ while(current <= right) {
 }
 
 
-LinkedList template: 
 
-function(node) :
 
-1.	check base case.
-2.  process current node, and possibly process node.next or node.next.next
-3.	recursive calls: function(node.next) or function(node.next.next)
+
+
 
 LinkedList cycle template:
 
@@ -257,6 +437,16 @@ while(slow != fast) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
 Topological sort template, DFS:
 
 1. Run DFS
@@ -278,12 +468,19 @@ dfs(Node current) {
 	}
 	set status to visited
 	
-	result.add(current); 
+	result.add(current);  // this is the time when the node is finished
 }
 	
 in main function:
- for all nodes: dfs(node)
-Collections.reverse(result)
+	for all nodes: dfs(node)
+	Collections.reverse(result)
+
+
+
+
+
+
+
 
 
 
@@ -309,6 +506,13 @@ for(int i = 1; i <= n-1; i++) {  // for each node
 }
 
 
+
+
+
+
+
+
+
 Dijkstra’s algorithm template: 
 
 This algorithm is to calculate single source, shortest path.
@@ -320,12 +524,14 @@ Initially, the distance to the starting node is 0 and the distance to any other 
 Time complexity: O(n + m logm)
 priority_queue<pair<int,int>> q; 
 for (int i = 1; i <= n; i++) { 
-distance[i] = INF;
+	distance[i] = INF;
 }
 distance[x] = 0; 
 q.push({0,x});
 while (!q.empty()) {
-	int a = q.top().second; q.pop(); if (processed[a]) continue; processed[a] = true;
+	int a = q.top().second; q.pop(); 
+	if (processed[a]) continue; 
+	processed[a] = true;
 	for (auto u : adj[a]) {
 		int b = u.first, w = u.second;
 		if (distance[a]+w < distance[b]) {
@@ -334,6 +540,12 @@ while (!q.empty()) {
 	       	}
 	} 
 }
+
+
+
+
+
+
 
 
 Floyd-Warshall Algorithm template:
@@ -369,4 +581,117 @@ for (int k = 1; k <= n; k++) {
 
 
 
+
+
+
+
+Important syntax:
+
+LinkedList: if (head == null || head.next == null) return head;
+
+int mod = N % k;
+int size = N / k;
+
+Tree: if (root == null) return 0;
+
+if (t1 == null && t2 == null) return null;
+if (t1 == null) return t2;
+if (t2 == null) return t1;
+
+
+// check if left or right child is null before adding them into some list or set 
+if (node.left != null) queue.add(node.left);
+if (node.right != null) queue.add(node.right);
+
+// check if a node is null before doing root.left or root.right
+
+return new int[]{a, b, c};
+
+
+// array sort, lowercase
+int[] cnts = new int[26];
+for (char c : s.toCharArray()) {
+        cnts[c - 'a']++;
+    }
+
+// array sort, lowercase and uppercase
+int[] cnts = new int[256];
+for (char c : s.toCharArray()) {
+        cnts[c]++;
+    }
+
+int[][] nums
+int row = nums.length, col = nums[0].length;
+nums[row][col]
+
+PriorityQueue<Integer> pq = new PriorityQueue<>();
+Add, contains, offer, peek, poll, size, remove
+
+Queue<Integer> queue = new LinkedList<>();
+Add, offer, peek, poll, remove
+
+Stack<Integer> stack = new Stack<>();
+Empty(), peek, pop, push
+ 
+Integer.MAX_VALUE
+Integer.MIN_VALUE
+
+Arrays.sort(nums)
+
+// sort by end time
+Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+
+Arrays.sort(intervals, new Comparator<int[]>() {
+     @Override
+     public int compare(int[] o1, int[] o2) {
+         return (o1[1] < o2[1]) ? -1 : ((o1[1] == o2[1]) ? 0 : 1);
+     }
+});
+
+//  sorting with two criteria
+Arrays.sort(people, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+
+Math.max(a, b)
+
+Math.min(a, b)
+
+for(int num : nums) {
+}
+
+for(char c : s.toCharArray()) {
+}
+
+Return condition ？ A ： B;
+
+StringBuilder sb = new StringBuilder();
+sb.append(“a string”);
+
+
+backtrack: 
+List<List<Integer>> res = new ArrayList<List<Integer>>();
+List<Integer> current_list = new ArrayList<Integer>();
+
+
+int[] nums 
+if(nums == null || nums.length == 0) base case
+
+String to char array: string.toArray()
+
+Char array to string: String a = new String(char array); String a = String.valueof(char array)
+
+
+Building graph as adjacency list representation:
+
+// initialize the adjacency list
+List<Integer>[] graph = new List[numCourses];
+
+// initialize the list for each node
+for(int i = 0; i < numCourses; i++) {
+    graph[i] = new ArrayList<Integer>();
+}
+
+// add the edges into the list
+for(int[] pre : prerequisites) {
+    graph[pre[1]].add(pre[0]);
+}
 
